344a483edb053683677dde4879ce8859a633df63
fcarlone <frankcarlone@gmail.com>
2019-05-03 16:40:01 -0400

    Create route for html files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+
+// Routing
+module.exports = function (app) {
+
+  // Setup home route
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  });
+
+  // Setup survey route
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+  });
+
+  // Catch-all - no matching route default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.__dirname, "../public/home.html")
+  });
+
+};
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 
 let app = express();
 let PORT = process.env.PORT || 3000;
@@ -9,17 +8,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// Setup home route
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/home.html"))
-});
-
-// Setup survey route
-app.get("/survey", function (req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/survey.html"))
-});
+// Router
+require("./app/routing/htmlRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
 
 
+// Add listener
 app.listen(PORT, function () {
   console.log(`Listening on PORT: ${PORT}`)
 });
