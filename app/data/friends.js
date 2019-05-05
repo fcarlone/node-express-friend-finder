2ed99d81f73c60062698798950ec2298127c9994
fcarlone <frankcarlone@gmail.com>
2019-05-05 17:08:11 -0400

    Add seed friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,50 +2,95 @@
 
 let friendsArray = [
   {
-    "name": "Ahmed",
-    "photo": "https://s3-us-west-2.amazonaws.com/s3.shermans.test/wlnkfm/styles/alpha__380x250/s3/Kermit.jpg?itok=L0FKx4df",
+    "name": "Miss Piggy",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/2/22/MissPiggy.jpg",
     "scores": [
+      1,
+      3,
       5,
       1,
-      4,
-      4,
+      3,
       5,
       1,
-      2,
+      3,
       5,
-      4,
       1
     ]
   },
   {
-    "name": "Kermit",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Kermit the Frog",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/6/62/Kermit_the_Frog.jpg",
     "scores": [
-      4,
-      5,
-      3,
-      5,
-      4,
-      5,
       1,
+      2,
+      3,
+      4,
+      5,
+      5,
       4,
       3,
-      5
+      2,
+      1
     ]
   }, {
     "name": "Gonzo",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/0/0a/Gonzo_the_Great.jpg",
     "scores": [
-      1,
-      2,
       5,
       4,
+      3,
+      2,
+      1,
       5,
+      4,
+      2,
+      3,
+      1
+    ]
+  }, {
+    "name": "Fozzie Bear",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/5/51/Fozzie_Bear.jpg",
+    "scores": [
+      2,
+      4,
+      2,
+      4,
+      1,
+      3,
+      5,
+      1,
+      3,
+      4
+    ]
+  }, {
+    "name": "The Swedish Chef",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/e/e7/The_Swedish_Chef.jpg",
+    "scores": [
+      1,
       2,
       3,
       4,
       5,
-      3
+      4,
+      3,
+      2,
+      1,
+      1
+    ]
+  }, {
+    "name": "Beaker",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/5/59/Beaker_%28Muppet%29.jpg",
+    "scores": [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
     ]
   }
 ];
