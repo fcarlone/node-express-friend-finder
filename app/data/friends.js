a79a16f5106a6548dbff9b5f3038693f4e9a5511
fcarlone <frankcarlone@gmail.com>
2019-05-05 09:26:50 -0400

    Create friend finder function

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,37 @@ let friendsArray = [
       4,
       1
     ]
+  },
+  {
+    "name": "Kermit",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      4,
+      5,
+      3,
+      5,
+      4,
+      5,
+      1,
+      4,
+      3,
+      5
+    ]
+  }, {
+    "name": "Gonzo",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      1,
+      2,
+      5,
+      4,
+      5,
+      2,
+      3,
+      4,
+      5,
+      3
+    ]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,67 @@ module.exports = function (app) {
 
   // API POST requests
   app.post("/api/friends", function (req, res) {
-    console.log(req.body)
-    res.json(friendsTable);
-  })
+    // console.log(req.body)
+    // User input array
+    let userInput = req.body
+
+    handleFriendFinderLogic(userInput)
+
+    // Friends data array
+    // let friendsDataArray = friendsTable
+
+    // friendsDataArray.forEach((friend) => {
+    //   let i = 0;
+    //   console.log(friend.name)
+    //   friend.scores.forEach((score) => {
+
+    //     console.log(score);
+    //     console.log(userInput.scores[i])
+    //     console.log(`${friend.name} score: ${score} - User input score ${userInput.scores[i]} = ${score - userInput.scores[i]}`)
+    //     i++
+    //   });
+    //   console.log('------------------')
+    // })
+    // res.json(friendsTable);
+    // friendsTable.push(req.body)
+
+  });
+
+  // Return friend match
+  const handleFriendFinderLogic = (userInput) => {
+    let friendMatch;
+    let friendSum;
+
+    console.log(userInput)
+
+    // Friends data array
+    let friendsDataArray = friendsTable
+
+    // Interatre through friendsArray
+    friendsDataArray.forEach((friend) => {
+      let sum = 0;
+      let i = 0;
+
+      // Get friend score
+      friend.scores.forEach((score) => {
+        let scoreCalculation = Math.abs(score - userInput.scores[i]);
+        console.log(scoreCalculation);
+        sum += scoreCalculation
+        i++;
+      });
+
+      console.log('SUM: ', sum)
+      console.log('FRIEND SUM: ', friendSum)
+      // Compare freinds score
+      if (sum <= friendSum || friendSum === undefined) {
+        friendSum = sum;
+        friendMatch = friend;
+      };
+
+      console.log('------------------')
+
+    })
+
+    console.log('friend match', friendMatch)
+  }
 };
